9d425e57bbc0752baf846326553d29e864610ae1
Dinesh <sunku.dinesh9@gmail.com>
2019-12-24 22:29:54 +0530

    Adding JerseyDemo Project

---
+++ src/main/java/com/accenture/demo/JerseyDemoProject/AlianRepository.java
@@ -0,0 +1,37 @@
+package com.accenture.demo.JerseyDemoProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlianRepository {
+	public List<Alian> alian;
+
+	public AlianRepository() {
+		alian = new ArrayList<Alian>();
+
+		Alian a1 = new Alian();
+		a1.setName("Dinesh");
+		a1.setPoints(10);
+
+		Alian a2 = new Alian();
+		a2.setName("Tharun");
+		a2.setPoints(5);
+
+		alian.add(a1);
+		alian.add(a2);
+	}
+
+	public List<Alian> getAlians() {
+		return alian;
+	}
+
+	public Alian getAlian(String name) {
+		for (Alian alian : alian) {
+			if (alian.getName().equalsIgnoreCase("Dinesh")) {
+				return alian;
+			}
+		}
+		return null;
+
+	}
+}
