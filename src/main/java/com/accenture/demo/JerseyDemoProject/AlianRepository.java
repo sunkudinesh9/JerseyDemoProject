06e5597edfe17920e95df8b377633cd224754914
Dinesh <sunku.dinesh9@gmail.com>
2019-12-25 16:37:34 +0530

    Adding JDBC code

--- src/main/java/com/accenture/demo/JerseyDemoProject/AlianRepository.java
+++ src/main/java/com/accenture/demo/JerseyDemoProject/AlianRepository.java
@@ -1,41 +1,88 @@
 package com.accenture.demo.JerseyDemoProject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AlianRepository {
 	public List<Alian> alian;
 
+	Connection con = null;
+	Statement stmt = null;
+	ResultSet res = null;
+	PreparedStatement pstmt = null;
+
 	public AlianRepository() {
-		alian = new ArrayList<Alian>();
-
-		Alian a1 = new Alian();
-		a1.setName("Dinesh");
-		a1.setPoints(10);
-
-		Alian a2 = new Alian();
-		a2.setName("Tharun");
-		a2.setPoints(5);
-
-		alian.add(a1);
-		alian.add(a2);
+		String url = "jdbc:mysql://localhost:3306/jerseyproject";
+		String username = "root";
+		String password = "root";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(url, username, password);
+			System.out.println("Connected to Datatabase...........");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public List<Alian> getAlians() {
-		return alian;
+		List<Alian> alianList = new ArrayList<Alian>();
+		try {
+			stmt = con.createStatement();
+			String sql = "select * from empdata";
+			res = stmt.executeQuery(sql);
+			while (res.next()) {
+				Alian alian = new Alian();
+				alian.setName(res.getString(1));
+				alian.setPoints(res.getInt(2));
+				alianList.add(alian);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return alianList;
 	}
 
 	public Alian getAlian(String name) {
-		for (Alian alian : alian) {
-			if (alian.getName().equalsIgnoreCase(name)) {
-				return alian;
+		Alian alian = new Alian();
+		try {
+			String sql = "select * from empdata where name=" + name;
+			stmt = con.createStatement();
+			res = stmt.executeQuery(sql);
+			if (res.next()) {
+				alian.setName(res.getString(0));
+				alian.setPoints(res.getInt(1));
 			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		return new Alian();
+		return alian;
 
 	}
 
 	public void createAlian(Alian alian2) {
+		try {
+			String sql = "insert into empdata(name, points) values(?,?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(0, alian2.getName());
+			pstmt.setInt(1, alian2.getPoints());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		alian.add(alian2);
 	}
 }
--- src/main/java/com/accenture/demo/JerseyDemoProject/AlianResource.java
+++ src/main/java/com/accenture/demo/JerseyDemoProject/AlianResource.java
@@ -14,7 +14,7 @@ public class AlianResource {
 	AlianRepository alianaRepository = new AlianRepository();
 
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public List<Alian> getAlians() {
 
 		return alianaRepository.getAlians();
@@ -22,7 +22,7 @@ public class AlianResource {
 
 	@GET
 	@Path("getalian/{name}")
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	public Alian getAlian(@PathParam("name") String name) {
 
 		return alianaRepository.getAlian(name);
@@ -30,7 +30,7 @@ public class AlianResource {
 
 	@POST
 	@Path("/createalian")
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	public Alian createAlian(Alian alian) {
 		alianaRepository.createAlian(alian);
 		return alian;
